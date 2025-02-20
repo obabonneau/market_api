be18c8bb3eddbf5bc0e3d0a0209a7d4b1a527d65
arnaud-coding <arnaudberthollet@hotmail.fr>
2025-02-20 20:30:29 +0100

    Créer commande

--- app/Controllers/CommandeController.php
+++ app/Controllers/CommandeController.php
@@ -84,4 +84,67 @@ class CommandeController
             echo json_encode("ERREUR : Méthode non autorisée !");
         }
     }
+
+    // -------------------------------
+    // METHODE POUR CREER UNE COMMANDE
+    // -------------------------------
+    public function add()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Methods: POST");
+        header("Access-Control-Max-Age: 3600");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $rawData = file_get_contents('php://input');
+            $data = json_decode($rawData, true);
+            $id_client = $data['id_client'] ?? null;
+            $id_statut = $data['id_statut'] ?? null;
+            $num_commande = $data['num_commande'] ?? null;
+            $date_commande = $data['date_commande'] ?? null;
+            $produits = $data['produits'] ?? [];
+
+            if ($id_client && $id_statut && $num_commande && $date_commande  && count($produits) !== 0) {
+
+                $commande = new Commande();
+                $commande->setId_client($id_client);
+                $commande->setId_statut($id_statut);
+                $commande->setNum_commande($num_commande);
+                $commande->setDate_commande($date_commande);
+
+                $commandeModel = new CommandeModel();
+                $lastId = $commandeModel->createClientOrder($commande);
+
+                if ($lastId) {
+
+                    $id_commande = $lastId;
+
+                    foreach ($produits as  $produit) {
+
+                        $id_produit = $produit['id_produit'];
+                        $quantite = $produit['quantite'];
+                        $success = $commandeModel->addProductToOrder($id_produit, $id_commande, $quantite);
+                    }
+                    if ($success) {
+                        http_response_code(201); // 200 OK
+                        echo json_encode(["message" => "Commande crée avec succès !"]);
+                    } else {
+                        http_response_code(503); // 503 Service Unavailable
+                        echo json_encode(["message" => "ERREUR lors de la création de la commande !"]);
+                    }
+                } else {
+                    http_response_code(503); // 503 Service Unavailable
+                    echo json_encode(["message" => "ERREUR lors de la création de la commande !"]);
+                }
+            } else {
+                http_response_code(400); // 400 Bad Request
+                echo json_encode(["message" => "Paramètres manquants !"]);
+            }
+        } else {
+            http_response_code(405); // 405 Method Not Allowed
+            echo json_encode("Méthode non autorisée !");
+        }
+    }
 }
--- app/Models/CommandeModel.php
+++ app/Models/CommandeModel.php
@@ -71,4 +71,60 @@ class CommandeModel extends DbConnect
             //die;
         }
     }
+
+    // -------------------------------
+    // METHODE POUR CREER UNE COMMANDE ET LA RELIER A UN CLIENT
+    // (LA COMMANDE SERA "REMPLIE" DE PRODUITS VIA UNE AUTRE METHODE)
+    // -------------------------------
+    public function createClientOrder(Commande $commande)
+    {
+        try {
+            // PREPARATION DE LA REQUETE SQL
+            $this->request = $this->connection->prepare("INSERT INTO com_commande
+                                                         VALUES
+                                                         (NULL,
+                                                          :id_statut,
+                                                          :id_client,
+                                                          :num_commande,
+                                                          :date_commande)");
+
+            $this->request->bindValue(':id_statut', $commande->getId_statut());
+            $this->request->bindValue(':id_client', $commande->getId_client());
+            $this->request->bindValue(':num_commande', $commande->getNum_commande());
+            $this->request->bindValue(':date_commande', $commande->getDate_commande());
+
+            // EXECUTION DE LA REQUETE SQL
+            $this->request->execute();
+
+            // Récupération de l'ID de la commande crée avec lastInsertId()
+            $lastId = $this->connection->lastInsertId();
+            return $lastId;
+        } catch (PDOException $e) {
+            //echo $e->getMessage();
+            //die;
+        }
+    }
+
+    // -------------------------------
+    // METHODE POUR AJOUTER UN PRODUIT DANS UNE COMMANDE (ICI, PAS DE LIEN AVEC UN CLIENT)
+    // -------------------------------
+    public function addProductToOrder($id_produit, $id_commande, $quantite)
+    {
+
+        try {
+            // PREPARATION DE LA REQUETE SQL
+            $this->request = $this->connection->prepare("INSERT INTO com_commande
+                                                         VALUES (NULL, :id_produit, :id_commande, :quantite)");
+
+            $this->request->bindValue(':id_produit', $id_produit);
+            $this->request->bindValue(':id_commande', $id_commande);
+            $this->request->bindValue(':quantite', $quantite);
+
+            // EXECUTION DE LA REQUETE SQL
+            return $this->request->execute();
+        } catch (PDOException $e) {
+            //echo $e->getMessage();
+            //die;
+        }
+    }
 }
